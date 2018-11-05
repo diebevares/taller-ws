bb534dcb8e5603dc597ea07d3f9518a0c104d3ed
diebevares <35883469+diebevares@users.noreply.github.com>
2018-11-04 20:07:31 -0600

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -2,10 +2,10 @@
 return [
 	'settings' => [
 		'db' => [
-			'host' => '192.168.1.250',
-			'dbname' => 'taller',
-			'user' => 'taller',
-			'pass' => 'T4113r'
+			'host' => '',
+			'dbname' => '',
+			'user' => '',
+			'pass' => ''
 		]
 	]
 ]
